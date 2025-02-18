e82f5807ec726f3a889f6affd6803b8b56c01bf9
Mukesh Silwal <mukesh.silwal@citytech.global>
2025-02-18 13:32:14 +0545

    "Test cases removed

--- src/main/java/com/Transaction/transaction/config/SuperUserInitializer.java
+++ src/main/java/com/Transaction/transaction/config/SuperUserInitializer.java
@@ -1,31 +1,31 @@
-//package com.Transaction.transaction.config;
-//
-//import com.Transaction.transaction.entity.Role1;
-//import com.Transaction.transaction.entity.Users;
-//import com.Transaction.transaction.repository.UserRepo;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//@RequiredArgsConstructor
-//@Component
-//public class SuperUserInitializer implements ApplicationRunner {
-//
-//    private final UserRepo userRepository;
-//    private final PasswordEncoder encoder;
-//
-//    @Override
-//    public void run(ApplicationArguments args) {
-//        if (!userRepository.existsByEmail("mukeshsilwal5@gmail.com")) {
-//            Users superUser = new Users();
-//            superUser.setEmail("mukeshsilwal5@gmail.com");
-//            superUser.setPassword(encoder.encode("123456"));
-//            superUser.setRole1(Role1.SUPER_ADMIN);
-//            superUser.setRole(Role1.SUPER_ADMIN);
-//            userRepository.save(superUser);
-//        }
-//    }
-//}
-//
+package com.Transaction.transaction.config;
+
+import com.Transaction.transaction.entity.Role;
+import com.Transaction.transaction.entity.Users;
+import com.Transaction.transaction.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class SuperUserInitializer implements ApplicationRunner {
+
+    private final UserRepo userRepository;
+    private final PasswordEncoder encoder;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (!userRepository.existsByEmail("mukeshsilwal5@gmail.com")) {
+            Users superUser = new Users();
+            superUser.setEmail("mukeshsilwal5@gmail.com");
+            superUser.setPassword(encoder.encode("123456"));
+            superUser.setRole1(Role.SUPER_ADMIN);
+            superUser.setRole(Role.SUPER_ADMIN);
+            userRepository.save(superUser);
+        }
+    }
+}
+
