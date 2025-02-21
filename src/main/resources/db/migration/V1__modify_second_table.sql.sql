2e5ff1c58125597fc79f1104ca2b40286e248416
Mukesh Silwal <mukesh.silwal@citytech.global>
2025-02-21 13:21:05 +0545

    "modify code

--- src/main/resources/db/migration/V1__modify_second_table.sql.sql
+++ src/main/resources/db/migration/V1__modify_second_table.sql.sql
@@ -66,5 +66,6 @@ CREATE TABLE IF NOT EXISTS users_table (
                                            username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    role VARCHAR(255) NOT NULL,
+    otp VARCHAR(255)
     );
